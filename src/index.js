bb8b19f15e0351f3f331ed027c9745f5b8085b65
Joseph Orbegoso Pea <joe@trusktr.io>
2017-05-16 16:38:36 -0700

    Rewrote the Class helper, now classes can have private and protected members.

--- src/index.js
+++ src/index.js
@@ -1,109 +1,150 @@
-/**
- * ClassBody is an object literal that you can pass into `Class()` (when making
- * classes that don't extend anything) or `Class().extend()` when extending
- * classes.
- *
- * @typedef {ClassBody}
- * @property {Function} constructor The constructor to use for your class. It
- * can be set on the `constructor` property, or on a property matching the name
- * of your class. If none of those are set, then a default constructor will be
- * made for you a la ES6 (it calls super() for you).
- */
+const publicProtoProtectedProtoMap = new WeakMap
+const protectedProtoPublicProtoMap = new WeakMap
+const privateProtoPublicProtoMap = new WeakMap
 
-// Holds the name of the new class being created whenever Class is called, in order for extend() to use it.
-let className = ''
+const instanceProtectedMap = new WeakMap
+const protectedInstancePublicInstanceMap = new WeakMap
+const privateInstancePublicInstanceMap = new WeakMap
 
 /**
- * Creates a new class that extends parentClass. The new class will have the
- * properties and methods defined in body. Of the body has a constructor
- * property, then that becomes the constructor for the class, otherwise a
- * default constructor is created similarly to ES6 classes.
- *
- * @param {Function} parentClass The class that the new class will extend from.
- * @param {ClassBody} body The body that will be the properties and methods of the new class.
- * @return {Function} The new class constructor function.
+ * @param {string} definer Function for defining a class...
  */
-function extend(parentClass, body) {
-    console.log(' --- Class.extend')
-    // set a default constructor if one wasn't supplied, similar to ES6
-    // classes. Default constructors in ES6 call super() automatically.
-    if (!body.hasOwnProperty('constructor')) {
-        // Here we create the default named constructor.  Note that errors
-        // in this constructor might be hard to debug since the definition
-        // will appear in a random VMXXXX in Chrome Inspector's Sources
-        // tab.
-        body.constructor = new Function('parentClass', `
+function Class(className, definer) {
+    "use strict"
+
+    if (typeof className == 'function' && !definer) {
+        definer = className
+        className = definer.name || ''
+    }
+    else if (typeof className != 'string' || typeof definer != 'function')
+        throw new Error('Invalid args.')
+
+    if (!definer || typeof definer != 'function')
+        throw new Error('You must specify a definer function.')
+
+    const ParentClass = this || Object
+
+    if (typeof ParentClass != 'function')
+        throw new Error('Invalid parent class.')
+
+    function protectedGetter(instance) {
+        if (!(instance instanceof NewClass || instance instanceof dummyProtectedCtor))
+            throw new Error('Invalid access.')
+
+        let currentPublicPrototype
+        if (instance instanceof NewClass) {
+            currentPublicPrototype = instance.constructor.prototype // TODO use getPrototypeOf
+        }
+        else {
+            currentPublicPrototype = protectedProtoPublicProtoMap.get(Object.getPrototypeOf(instance))
+        }
+
+        const currentProtectedPrototype = publicProtoProtectedProtoMap.get(currentPublicPrototype)
+        let _protected = instanceProtectedMap.get(instance)
+        let publics = protectedInstancePublicInstanceMap.get(instance)
+
+        if (!_protected && !publics) {
+            _protected = Object.create(currentProtectedPrototype)
+            instanceProtectedMap.set(instance, _protected)
+            protectedInstancePublicInstanceMap.set(_protected, instance)
+        }
+
+        if (!_protected && publics) {
+            return publics
+        }
+        else if (_protected && !publics) {
+            return _protected
+        }
+
+        return _protected
+    }
+
+    const publicPrototype = Object.create(ParentClass.prototype)
+    const privatePrototype = {}
+
+    const instancePrivatesMap = new WeakMap
+    function privatesGetter(instance) {
+        if (!(Object.getPrototypeOf(instance) === publicPrototype || Object.getPrototypeOf(instance) === privatePrototype))
+            throw new Error('Invalid access.')
+
+        let privates = instancePrivatesMap.get(instance)
+        let publics = privateInstancePublicInstanceMap.get(instance)
+
+        if (!privates && !publics) {
+            privates = Object.create(privatePrototype)
+            instancePrivatesMap.set(instance, privates)
+            privateInstancePublicInstanceMap.set(privates, instance)
+        }
+
+        if (!privates && publics) {
+            return publics
+        }
+        else if (privates && !publics) {
+            return privates
+        }
+
+        return privates
+    }
+
+    const parentProtected = publicProtoProtectedProtoMap.get(ParentClass.prototype) || {}
+
+    const protectedPrototype = Object.create(parentProtected)
+    function dummyProtectedCtor() {}
+    dummyProtectedCtor.prototype = protectedPrototype
+
+    const def = definer(publicPrototype, protectedGetter, privatesGetter)
+
+    copyDescriptors(protectedGetter, protectedPrototype)
+    copyDescriptors(privatesGetter, privatePrototype)
+
+    if (typeof def == 'object') {
+        copyDescriptors(def.public, publicPrototype)
+        copyDescriptors(def.protected, protectedPrototype)
+        copyDescriptors(def.private, privatePrototype)
+        delete def.public
+        delete def.protected
+        delete def.private
+        copyDescriptors(def, publicPrototype)
+    }
+
+    if (!publicPrototype.hasOwnProperty('constructor')) {
+        publicPrototype.constructor = new Function('ParentClass', `
             return function ${className}() {
-                parentClass.apply(this, arguments)
+                ParentClass.apply(this, arguments)
             }
-        `)(parentClass)
-        //body.constructor = function() { parentClass.apply(this, arguments) }
+        `)(ParentClass)
     }
 
-    // keep a ref to the new class' constructor.
-    const originalCtor = body.constructor
-    delete body.constructor
-    let newClass = function() {
-        return originalCtor.apply(this, arguments)
-    }
+    const originalConstructor = publicPrototype.constructor
+    console.log('original', originalConstructor)
 
-    // the new class extends the parentClass
-    newClass.prototype = Object.create(parentClass.prototype)
-    newClass.prototype.constructor = newClass
+    var NewClass = new Function('originalConstructor', `
+        return function ${className}() {
+            return originalConstructor.apply(this, arguments)
+        }
+    `)(originalConstructor)
 
-    // Set the super helper. This lets us do, f.e.:
-    //
-    // ```
-    // this.super().apply(this, arguments)
-    // ```
-    //
-    // or
-    //
-    // ```
-    // this.super().someMethod.apply(this, arguments)
-    // ```
-    //
-    // without having to import the super class.
-    //
-    // TODO: Should we copy getters and setters? Reading and writing those
-    // might cause unexpected behavior.
-    if (Class.useSuperHelper) {
-        //newClass.prototype.super = function super() {
-            //return parentClass.prototype
-        //}
-    }
+    NewClass.subclass = Class
+    NewClass.prototype = publicPrototype
 
-    // apply all the properties and methods from the new class'
-    // body to the constructor's prototype. Remove the reference to
-    // the constructor from the body so we don't have to check for
-    // it in the iteration, for performance.
-    let keys = Object.keys(body)
-    let i = keys.length
-    while (i--) { // for..in is faster than a vanilla loop on Object.getOwnPropertyNames(body)
-        let descriptor = Object.getOwnPropertyDescriptor(body, keys[i])
-        Object.defineProperty(newClass.prototype, keys[i], descriptor)
-    }
+    // TODO: make non-writable and non-configurable like ES6
+    NewClass.prototype.constructor = NewClass
 
-    console.log(' --- returning new class:', new function() {})
-    return newClass
+    publicProtoProtectedProtoMap.set(publicPrototype, protectedPrototype)
+    protectedProtoPublicProtoMap.set(protectedPrototype, publicPrototype)
+    privateProtoPublicProtoMap.set(privatePrototype, publicPrototype)
+
+    return NewClass
 }
 
-/**
- * @param {string} name The name of the class you are creating.
- * @param {ClassBody} body An object containing the properties and method of your
- * class. They'll be assigned to the class' prototype.
- */
-function Class(name, body) {
-    console.log(' --- Class')
-    className = name || ''
-
-    if (!body) // Supplying no body means a .extends() call will be chained.
-        return { extends: extend }
-    else if (body) // supplying a body means we're defining a new class, extending Object by default.
-        return extend(Object, body)
+function copyDescriptors(source, destination) {
+    const props = Object.keys(source)
+    let i = props.length
+    while (i--) {
+        const prop = props[i]
+        const descriptor = Object.getOwnPropertyDescriptor(source, prop)
+        Object.defineProperty(destination, prop, descriptor)
+    }
 }
-
-// Don't use the super helper by default. Note, it only works in non-strict modes.
-Class.useSuperHelper = false
 
 module.exports = Class
