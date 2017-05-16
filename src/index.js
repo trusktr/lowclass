4f250230fb4850d0e3241c99b74ff3ad54f8a705
Joseph Orbegoso Pea <joe@trusktr.io>
2017-05-15 18:07:07 -0700

    Use only the constructor property (no longer properties with the same name as the class), and make it work with concise methods.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  *
  * @typedef {ClassBody}
  * @property {Function} constructor The constructor to use for your class. It
- * can be set on the `constructor` property, or ona property matching the name
+ * can be set on the `constructor` property, or on a property matching the name
  * of your class. If none of those are set, then a default constructor will be
  * made for you a la ES6 (it calls super() for you).
  */
@@ -24,29 +24,28 @@ let className = ''
  * @return {Function} The new class constructor function.
  */
 function extend(parentClass, body) {
+    console.log(' --- Class.extend')
     // set a default constructor if one wasn't supplied, similar to ES6
     // classes. Default constructors in ES6 call super() automatically.
     if (!body.hasOwnProperty('constructor')) {
-        if (body[className]) {
-            body.constructor = body[className]
-            delete body[className]
-        }
-        else {
-            // Here we create the default named constructor.  Note that errors
-            // in this constructor might be hard to debug since the definition
-            // will appear in a random VMXXXX in Chrome Inspector's Sources
-            // tab.
-            body.constructor = new Function('parentClass', `
-                return function ${className}() {
-                    parentClass.apply(this, arguments)
-                }
-            `)(parentClass)
-            //body.constructor = function() { parentClass.apply(this, arguments) }
-        }
+        // Here we create the default named constructor.  Note that errors
+        // in this constructor might be hard to debug since the definition
+        // will appear in a random VMXXXX in Chrome Inspector's Sources
+        // tab.
+        body.constructor = new Function('parentClass', `
+            return function ${className}() {
+                parentClass.apply(this, arguments)
+            }
+        `)(parentClass)
+        //body.constructor = function() { parentClass.apply(this, arguments) }
     }
 
     // keep a ref to the new class' constructor.
-    let newClass = body.constructor
+    const originalCtor = body.constructor
+    delete body.constructor
+    let newClass = function() {
+        return originalCtor.apply(this, arguments)
+    }
 
     // the new class extends the parentClass
     newClass.prototype = Object.create(parentClass.prototype)
@@ -69,29 +68,15 @@ function extend(parentClass, body) {
     // TODO: Should we copy getters and setters? Reading and writing those
     // might cause unexpected behavior.
     if (Class.useSuperHelper) {
-        //newClass.prototype.super = parentClass
-        function superHelper() {
-            return superHelper.caller.ownerClass
-        }
-        newClass.prototype.super = superHelper
-        // XXX: use getOwnPropertyNames instead? The super helper might need it
-        // in order to work with non-enumerable properties.
-        let keys = Object.keys(parentClass.prototype)
-        let i = keys.length
-        while (i--) { // This iteration on Object.keys is faster than all others by a ton (f.e. way faster than for..in), see https://gist.github.com/trusktr/d7e7a9d1161dd5a7971e.
-            let descriptor = Object.getOwnPropertyDescriptor(parentClass.prototype, keys[i])
-            Object.defineProperty(parentClass, keys[i], descriptor)
-
-            if (descriptor.value instanceof Function)
-                descriptor.value.ownerClass = parentClass
-        }
+        //newClass.prototype.super = function super() {
+            //return parentClass.prototype
+        //}
     }
 
     // apply all the properties and methods from the new class'
     // body to the constructor's prototype. Remove the reference to
     // the constructor from the body so we don't have to check for
     // it in the iteration, for performance.
-    delete body.constructor
     let keys = Object.keys(body)
     let i = keys.length
     while (i--) { // for..in is faster than a vanilla loop on Object.getOwnPropertyNames(body)
@@ -99,6 +84,7 @@ function extend(parentClass, body) {
         Object.defineProperty(newClass.prototype, keys[i], descriptor)
     }
 
+    console.log(' --- returning new class:', new function() {})
     return newClass
 }
 
@@ -108,6 +94,7 @@ function extend(parentClass, body) {
  * class. They'll be assigned to the class' prototype.
  */
 function Class(name, body) {
+    console.log(' --- Class')
     className = name || ''
 
     if (!body) // Supplying no body means a .extends() call will be chained.
