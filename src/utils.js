e06625ad1381e153a888babf684344d45684667c
Joe Pea <joe@trusktr.io>
2018-04-02 13:03:23 -0700

    update setDescriptor so it will be easier and less verbose to switch from values to accessors or vice versa

--- src/utils.js
+++ src/utils.js
@@ -12,8 +12,35 @@ function getFunctionBody( fn ) {
     return code.join("\n")
 }
 
+const descriptorDefaults = {
+    enumerable: true,
+    configurable: true,
+}
+
+// makes it easier and less verbose to work with descriptors
 function setDescriptor( obj, key, def ) {
-    const descriptor = Object.getOwnPropertyDescriptor( obj, key ) || def
-    if ( descriptor !== def ) Object.assign( descriptor, def )
-    Object.defineProperty( obj, key, descriptor )
+    const descriptor = Object.getOwnPropertyDescriptor( obj, key ) || {}
+
+    if ( ( def.get || def.set ) && (
+        typeof def.value !== 'undefined' ||
+        typeof def.writable !== 'undefined'
+    )) {
+        throw new TypeError('cannot specify both accessors and a value or writable attribute')
+    }
+
+    if ( def.get || def.set ) {
+        delete descriptor.value
+        delete descriptor.writable
+    }
+    else if (
+        typeof def.value !== 'undefined' ||
+        typeof def.writable !== 'undefined'
+    ) {
+        delete def.get
+        delete def.set
+    }
+
+    Object.defineProperty( obj, key,
+        Object.assign( {}, descriptorDefaults, descriptor, def )
+    )
 }
