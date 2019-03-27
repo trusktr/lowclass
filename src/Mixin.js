68f1c93f9a7dfd1332f4645bd6388ba0d1ce06ee
Joe Pea <joe@trusktr.io>
2019-03-27 00:13:42 -0700

    update/add some comments

--- src/Mixin.js
+++ src/Mixin.js
@@ -3,6 +3,7 @@ import Class from './'
 export default
 function Mixin( factory, Default ) {
 
+    // XXX Maybe Cached should go last.
     factory = Cached( factory )
     factory = HasInstance( factory )
     factory = Dedupe( factory )
@@ -68,7 +69,7 @@ function HasInstance( classFactory ) {
                 // it's own `[Symbol.hasInstance]()` method, therefore `this`
                 // will be the subclass, not this `Class`, when the prototype
                 // lookup on the subclass finds the `[Symbol.hasInstance]()`
-                // method of this `Class`. In this case, we don't wsnt to run
+                // method of this `Class`. In this case, we don't want to run
                 // our logic here, so we delegate to the super class of this
                 // `Class` to take over with the instanceof check. In many
                 // cases, the super class `[Symbol.hasInstance]()` method will
