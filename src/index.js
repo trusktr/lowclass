4b100041eff07f91f9901c03011dac05bf9b5f71
Joe Pea <joe@trusktr.io>
2019-04-06 11:00:06 -0700

    reorganize index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-export {default as Mixin} from './Mixin'
-export {default as instanceOf} from './instanceOf'
-export * from './native'
+// the bread and butter
 export * from './Class'
 export default from './Class'
+
+// mix and match your classes!
+export {default as Mixin} from './Mixin'
+
+// extras
+export {default as instanceOf} from './instanceOf'
+export * from './native'
+
 export const version = '4.5.2'
