dc284dcb525a86a6d024cc61be568b6c1cf08701
Joe Pea <joe@trusktr.io>
2019-11-27 10:43:31 -0800

    export multiple from the index file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import Class from './Class'
 export default Class
 
 // mix and match your classes!
+export * from './multiple'
 export * from './Mixin'
 
 // extras
-export { default as instanceOf } from './instanceOf'
+export {default as instanceOf} from './instanceOf'
 export * from './native'
 export * from './utils'
 
