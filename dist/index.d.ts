78534900f5dfc2f0038b08d8cb338e33b0beeb01
Joe Pea <joe@trusktr.io>
2024-09-17 11:45:30 -0700

    v8.0.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
-export declare const version = "8.0.0";
+export declare const version = "8.0.1";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-export const version = '8.0.0';
+export const version = '8.0.1';
 //# sourceMappingURL=index.js.map
